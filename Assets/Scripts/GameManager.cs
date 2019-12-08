0604f4ca86fca36e410d65373c27ca886846c210
And1210 <amf7crazy@gmail.com>
2019-12-08 01:05:14 -0500

    Lets get this bread

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject Player;
+    Camera mainCamera;
+
+    //Block information for the gamespace
+    public GameObject Block;
+    public Material Completed;
+    public Material Waiting;
+
+    //Information of the gamespace
+    public int size;
+    public int blockWidth;
+    public int distance;
+
+    //Used variables by the game
+    GameObject[,] Bricks;
+    GameObject player;
+
+    void Start() {
+        mainCamera = Camera.main;
+
+        //Setting up the gamegrid
+        Bricks = new GameObject[size, size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int row = (i-size/2)*blockWidth;
+                int col = (j-size/2)*blockWidth;
+                Bricks[i,j] = Instantiate(Block, new Vector3(row, col, distance), Quaternion.identity);
+            }
+        }
+
+        //Spawning in the player
+        player = Instantiate(Player, new Vector3(0, 0, distance - blockWidth - 0.5f), Quaternion.identity);
+    }
+
+    void Update() {
+        Transform playerTransform = player.GetComponent<Transform>();
+        Vector3 pos = playerTransform.position;
+
+        //Setting any blocks underneath the player to be "completed"
+        int row = (int)(Mathf.Round(pos.x/blockWidth) + size/2);
+        int col = (int)(Mathf.Round(pos.y/blockWidth) + size/2);
+        Bricks[row, col].GetComponent<MeshRenderer>().material = Completed;
+
+        mainCamera.GetComponent<Transform>().position = new Vector3(pos.x, pos.y, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+    private bool moving = false;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (!moving) {
+            if (horizontal >= 0.5) { //right
+                StartCoroutine(Jump(0));
+            } else if (horizontal <= -0.5) { //left
+                StartCoroutine(Jump(1));
+            } else if (vertical >= 0.5) { //up
+                StartCoroutine(Jump(2));
+            } else if (vertical <= -0.5) { //down
+                StartCoroutine(Jump(3));
+            }
+        }
+    }
+
+    IEnumerator Jump(int dir) {
+        moving = true;
+
+        Vector3 relativeTarget = new Vector3(dir <= 1 ? -(2*dir-1) : 0, dir >= 2 ? -(2*(dir-2)-1) : 0, 0);
+        Vector3 target = transform.position + relativeTarget;
+        rb.AddForce(0, 0, -150);
+        yield return new WaitForSeconds(0.1f);
+
+        float step = speed * Time.deltaTime;
+        while (Vector3.Magnitude(transform.position-target) > 0.01) {
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position.Set(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), transform.position.z);
+
+        yield return new WaitForFixedUpdate();
+
+        moving = false;
+    }
+}
