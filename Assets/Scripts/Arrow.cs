ff75d1f1c2090b3af7f00d89a395a41c0f75cd33
And1210 <amf7crazy@gmail.com>
2019-12-12 02:55:12 -0500

    Map fill is complete

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,24 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Arrow {
 
     int row;
     int col;
     float rotation;
+    bool real;
 
-    public Arrow(int r, int c, float rot) {
+    GameObject brickRef;
+
+    public Arrow(int r, int c, float rot, ref GameObject brick, bool isReal) {
         row = r;
         col = c;
         rotation = rot;
+        real = isReal;
+
+        brickRef = brick;
     }
 
-    int GetRow() {
+    public void ChangeMaterial(Material m) {
+        brickRef.GetComponent<MeshRenderer>().material = m;
+    }
+
+    public GameObject SetOverlay(Material m) {
+        Vector3 pos = brickRef.GetComponent<Transform>().position;
+        pos.z -= 0.01f;
+
+        GameObject overlayRef = GameObject.Instantiate(brickRef, pos, Quaternion.AngleAxis(rotation, new Vector3(0, 0, -1)));
+        overlayRef.GetComponent<MeshRenderer>().material = m;
+
+        return overlayRef;
+    }
+
+
+    public int GetRow() {
         return row;
     }
-    int GetCol() {
+    public int GetCol() {
         return col;
     }
-    float GetRot() {
+    public float GetRot() {
         return rotation;
+    }
+    public bool IsReal() {
+        return real;
+    }
+
+    public void SetRot(float angle) {
+        rotation = angle;
+    }
+    public void MakeReal() {
+        real = true;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,8 +20,10 @@ public class GameManager : MonoBehaviour {
 
     //Used variables by the game
     GameObject[,] Bricks;
+    GameObject[,] Overlays;
     Arrow[,] Arrows;
     GameObject player;
+    Vector2 playerPos;
     private Vector2 targetIndex;
 
     void Start() {
@@ -29,6 +31,7 @@ public class GameManager : MonoBehaviour {
 
         //Setting up the gamegrid
         Bricks = new GameObject[size, size];
+        Overlays = new GameObject[size, size];
         Arrows = new Arrow[size, size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -36,18 +39,28 @@ public class GameManager : MonoBehaviour {
                 int col = (j-size/2)*blockWidth;
 
                 Bricks[i, j] = Instantiate(Block, new Vector3(row, col, distance), Quaternion.identity);
-                ChangeMaterial(Bricks[i, j], Waiting);
+                Overlays[i, j] = null;
 
-                Arrows[i, j] = new Arrow(i, j, 0);
+                Arrows[i, j] = new Arrow(i, j, 0, ref Bricks[i,j], false);
+                Arrows[i, j].ChangeMaterial(Waiting);
             }
         }
 
-        ChangeMaterial(Bricks[size - 5, size - 3], Target);
+        Arrows[size-6, size-3].ChangeMaterial(Target);
         targetIndex = new Vector2(size - 5, size - 3);
+        Overlays[size-6, size-3] = Arrows[size-6, size-3].SetOverlay(Target);
 
         //Spawning in the player
         player = Instantiate(Player, new Vector3(0, 0, distance - blockWidth - 0.5f), Quaternion.identity);
         player.GetComponent<Player>().SetGrid(Bricks);
+        Transform playerTransform = player.GetComponent<Transform>();
+        Vector3 pos = playerTransform.position;
+        playerPos = new Vector2((int)(Mathf.Round(pos.x/blockWidth) + size/2), (int)(Mathf.Round(pos.y/blockWidth) + size/2));
+
+        GetPath(new Vector2((int)playerPos.x, (int)playerPos.y), new Vector2(size - 5, size - 3));
+        FillGrid();
+        // Debug.Log(size*size);
+        // Debug.Log(GetEmpty());
     }
 
     void Update() {
@@ -57,20 +70,73 @@ public class GameManager : MonoBehaviour {
         //Setting any blocks underneath the player to be "completed"
         int i = (int)(Mathf.Round(pos.x/blockWidth) + size/2);
         int j = (int)(Mathf.Round(pos.y/blockWidth) + size/2);
-        ChangeMaterial(Bricks[i, j], Completed);
+        playerPos.Set(i, j);
+
+        Arrows[i, j].ChangeMaterial(Completed);
 
         mainCamera.GetComponent<Transform>().position = new Vector3(pos.x, pos.y, 0);
     }
 
-    //Vector2[] GetPath(Vector2 playerIndex, Vector2 targetIndex) {
-    //    List<Vector2> output = new List<Vector2>();
+    void GetPath(Vector2 playerIndex, Vector2 targetIndex) {
+        List<Vector2> output = new List<Vector2>();
 
-    //    int i = playerIndex.x;
-    //    int j = playerIndex.y;
+        int i = (int)playerIndex.x;
+        int j = (int)playerIndex.y;
 
-    //}
+        Vector2 step = new Vector2(0, 0);
+        while (i != targetIndex.x && j != targetIndex.y) {
+            int iDiff = (int)targetIndex.x - i;
+            int jDiff = (int)targetIndex.y - j;
 
-    void ChangeMaterial(GameObject go, Material m) {
-        go.GetComponent<MeshRenderer>().material = m;
+            if (iDiff / jDiff > 1) {
+                step.Set(iDiff/Mathf.Abs(iDiff), 0);
+            } else {
+                step.Set(0, jDiff/Mathf.Abs(jDiff));
+            }
+
+            // Debug.Log(i + " " + j + " " + step);
+
+            Arrows[i, j].SetRot(Vector2.Angle(new Vector2(0, 1), step));
+            Arrows[i, j].MakeReal();
+
+            Overlays[i, j] = Arrows[i, j].SetOverlay(Pointer);
+            i += (int)step.x;
+            j += (int)step.y;
+        }
+    }
+
+    void FillGrid() {
+        int empty = GetEmpty();
+        while(empty > 0) {
+            for (int i = 0; i < Overlays.GetLength(0); i++) {
+                for (int j = 0; j < Overlays.GetLength(1); j++) {
+                    for (int k = 1; k > -3; k--) {
+                        if (Overlays[i, j] != null)
+                            break;
+                        int i_t = (int)Mathf.Max(Mathf.Min(Overlays.GetLength(0)-1, i + k % 2), 0);
+                        int j_t = (int)Mathf.Max(Mathf.Min(Overlays.GetLength(1)-1, j + -1*((k+1) % 2)), 0);
+                        if (Overlays[i_t, j_t] != null) {
+                            Arrows[i, j].SetRot((Arrows[i_t, j_t].GetRot() + 180) % 360);
+                            Overlays[i, j] = Arrows[i, j].SetOverlay(Pointer);
+                            break;
+                        }
+                    }
+                }
+            }
+            empty = GetEmpty();
+        }
+    }
+
+    int GetEmpty() {
+        int notFilled = 0;
+        for (int i = 0; i < Overlays.GetLength(0); i++) {
+            for (int j = 0; j < Overlays.GetLength(1); j++) {
+                if (Overlays[i, j] == null) {
+                    notFilled++;
+                }
+            }
+        }
+
+        return notFilled;
     }
 }
